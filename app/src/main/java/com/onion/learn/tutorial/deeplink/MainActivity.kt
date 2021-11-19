663daec23b86353681537bf2212060f158cb2c8c
skullzero@163.com <skullzero@163.com>
2021-11-19 14:46:31 +0800

    Initial commit

---
+++ app/src/main/java/com/onion/learn/tutorial/deeplink/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.onion.learn.tutorial.deeplink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.loadUrl("file:///android_asset/deeplink_test.html")
+    }
+}
